d7b497e0999725e7ed3808e353bc98e5259accf4
Yeah-Kun <767786685@qq.com>
2017-08-31 19:31:09 +0800

    更新部署到服务器上的内容

--- SRSYS/SRSYS/urls.py
+++ SRSYS/SRSYS/urls.py
@@ -1,33 +1,11 @@
-"""SRSYS URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
-from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from . import view
-from . import login
+from . import views
 
-  
 urlpatterns = [
-    url(r'^$', view.index,name='index'),
-    url(r'^ReturnIMG1$', view.ReturnIMG1,name='ReturnIMG1'),
-    url(r'^ReturnIMG2$', view.ReturnIMG2,name='ReturnIMG2'),
-    url(r'^code$', view.code,name='code'),
-    url(r'^login$', login.Login,name='login'),
-    url(r'^admin/', admin.site.urls),
-
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    url(r'^$', views.index, name='index'), # name用于反向解析
+    url(r'^code/$', views.code,name='code'), # 验证码
+    url(r'^result/$',views.result,name='result') # 成绩展示页面
+    # url(r'^create_code/$',views.create_code_img), # 自己写的验证码程序
+]
